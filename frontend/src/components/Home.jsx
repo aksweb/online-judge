30d804cc96d11f3e9869e2d2d265e70a88e610c3
aksweb <21it3004@rgipt.ac.in>
2024-07-21 20:35:41 +0530

    home update

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -182,17 +182,19 @@ const Home = () => {
                             <strong>Created By:</strong>{" "}
                             {contest.createdBy.username}
                           </p>
-                          <button className="ml-auto">
-                            <Link
-                              to={`/view/${contest._id}`}
-                              className="text-red-500 relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-white rounded hover:bg-white group"
-                            >
-                              <span className="w-48 h-48 rounded rotate-[-40deg] bg-blue-700 absolute bottom-0 left-5 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
-                              <span className="relative w-full text-left text-black transition-colors duration-300 ease-in-out group-hover:text-white">
-                                View Contest
-                              </span>
-                            </Link>
-                          </button>
+                          {new Date() > new Date(contest.startTime) && (
+                            <button className="ml-auto">
+                              <Link
+                                to={`/view/${contest._id}`}
+                                className="text-red-500 relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-white rounded hover:bg-white group"
+                              >
+                                <span className="w-48 h-48 rounded rotate-[-40deg] bg-blue-700 absolute bottom-0 left-5 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
+                                <span className="relative w-full text-left text-black transition-colors duration-300 ease-in-out group-hover:text-white">
+                                  View Contest
+                                </span>
+                              </Link>
+                            </button>
+                          )}
                         </div>
                         <img
                           src="assets/down.png"
